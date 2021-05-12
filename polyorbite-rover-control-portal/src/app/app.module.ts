41bca483c5ce03af3356f7f924e251968264bae3
Thinking-Dragon <cfmgp@hotmail.com>
2021-05-11 20:27:53 -0400

    Declared video stream component in application module

--- polyorbite-rover-control-portal/src/app/app.module.ts
+++ polyorbite-rover-control-portal/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideoStreamComponent } from './video-stream/video-stream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoStreamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
