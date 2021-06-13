93d6256ef8abf12e24ef44948c4f0db84024451f
Thinking-Dragon <cfmgp@hotmail.com>
2021-06-13 00:58:12 -0400

    Interest point names are now editable from the dashboard

--- polyorbite-rover-control-portal/src/app/interest-point/component/list/interest-points.component.ts
+++ polyorbite-rover-control-portal/src/app/interest-point/component/list/interest-points.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { EditableTextComponent } from 'src/app/editable-text/editable-text.component';
 import { InterestPointService } from '../../service/interest-point.service';
 
 @Component({
@@ -8,6 +9,21 @@ import { InterestPointService } from '../../service/interest-point.service';
 })
 export class InterestPointsComponent {
   selectedId: string;
+
+  @ViewChildren('interestPointNameEditors') interestPointNameEditors: QueryList<EditableTextComponent>;
+
+  select(uuid: string): void {
+    if(uuid !== this.selectedId) {
+      this.stopEditing();
+      this.selectedId = uuid;
+    }
+  }
+
+  stopEditing(): void {
+    this.interestPointNameEditors.forEach(
+      editor => editor.isEditing = false
+    );
+  }
 
   constructor(public interestPoints: InterestPointService) {
     if(this.interestPoints.entries.length > 0) {
