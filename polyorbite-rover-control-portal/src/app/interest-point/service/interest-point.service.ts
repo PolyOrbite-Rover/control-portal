f1246a135a46428051fb93ba0300b78bc1ae67d7
Thinking-Dragon <cfmgp@hotmail.com>
2021-06-28 20:40:04 -0400

    Interest points service publishes to collector on add and update

--- polyorbite-rover-control-portal/src/app/interest-point/service/interest-point.service.ts
+++ polyorbite-rover-control-portal/src/app/interest-point/service/interest-point.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Message, Topic } from 'roslib';
+import { ROSService } from 'src/app/ROS/ros.service';
 import { InterestPoint } from '../interest-point';
 
 @Injectable({
@@ -7,10 +9,37 @@ import { InterestPoint } from '../interest-point';
 export class InterestPointService {
   add(interestPoint: InterestPoint): void {
     this.interestPoints.push(interestPoint);
+
+    interestPoint.onContentChanged.subscribe(
+      interestPoint => this.updateInterestPoint(interestPoint)
+    );
+    this.persistInterestPoint(interestPoint);
   }
 
   clear(): void {
     this.interestPoints = [];
+  }
+
+  private persistInterestPoint(interestPoint: InterestPoint): void {
+    const message = new Message({
+      uuid: interestPoint.uuid,
+      creationTimestamp: interestPoint.creationTimestamp,
+      name: interestPoint.name,
+      data: interestPoint.model.data
+    });
+
+    this.interestPointAddPublisher.publish(message);
+  }
+
+  private updateInterestPoint(interestPoint: InterestPoint): void {
+    const message = new Message({
+      uuid: interestPoint.uuid,
+      creationTimestamp: interestPoint.creationTimestamp,
+      name: interestPoint.name,
+      data: interestPoint.model.data
+    });
+
+    this.interestPointUpdatePublisher.publish(message);
   }
 
   get entries(): Array<InterestPoint> {
@@ -18,4 +47,11 @@ export class InterestPointService {
   }
 
   private interestPoints: Array<InterestPoint> = [];
+  private interestPointAddPublisher: Topic;
+  private interestPointUpdatePublisher: Topic;
+
+  constructor(private ros: ROSService) {
+    this.interestPointAddPublisher = ros.getTopic('interest_points/add', 'interest_points/InterestPoint');
+    this.interestPointUpdatePublisher = ros.getTopic('interest_points/update', 'interest_points/InterestPoint');
+  }
 }
